8e78b21bb47abbe9a4a7fd830518239c67f5de06
Nivesh Bansal <162882605+niveshbansal07@users.noreply.github.com>
2026-01-10 18:26:55 +0530

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css'
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Task & Time Tracker</title>
+</svelte:head>
+
+<slot />
+
