e41d8b057491f81b04930a27f94006ef1ba20c0f
Nivesh Bansal <162882605+niveshbansal07@users.noreply.github.com>
2026-01-11 12:58:32 +0530

    Integrate Home page layout
    Added a Navbar component and updated the page layout with new styles and content.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	let isAuth = false;
+</script>
+
+<div class="page">
+	<Navbar {isAuth} />
+
+	<div class="home-center">
+		<h2 class="big-btn">Made by <a href="https://nivesh-bansal.vercel.app/">Nivesh Bansal </a>❤️</h2>
+	</div>
+</div>
+
+<style>
+	.home-center {
+		height: 70vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.big-btn {
+		height: 60px;
+		padding: 0 40px;
+		font-size: 18px;
+	}
+
+	.home-center a{
+		text-decoration: none;
+		color: #5a2d82;
+	}
+</style>
