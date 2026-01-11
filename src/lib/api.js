4ac2e41fde005ece79f2896b4431cced33efa558
Nivesh Bansal <niveshbansal52@gmail.com>
2026-01-11 12:23:02 +0530

    API helper

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,1 +1,35 @@
-// 
+const API_URL = "http://localhost:5000/api";
+
+export async function signup(data) {
+    const res = await fetch(`${API_URL}/auth/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const json = await res.json();
+
+    if (!res.ok) {
+        throw new Error(json.message);
+    }
+
+    return json;
+}
+
+export async function login({ email, password }) {
+    const res = await fetch(`${API_URL}/auth/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message);
+
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+
+    return data;
+}
