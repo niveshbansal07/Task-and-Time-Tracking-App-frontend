0ecefc1abdf1ab8daeef87892a483740414ba4a2
Nivesh Bansal <162882605+niveshbansal07@users.noreply.github.com>
2026-01-12 06:13:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default {
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://task-and-time-tracking-app-backend.onrender.com/',
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	}
+};
