3a015c824d927ef3807f13dba80cae044535b9e0
Nivesh Bansal <162882605+niveshbansal07@users.noreply.github.com>
2026-01-12 06:04:36 +0530

    Change API URL to production endpoint

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://task-and-time-tracking-app-backend.onrender.com/api/";
 
 export async function signup(data) {
     const res = await fetch(`${API_URL}/auth/signup`, {
